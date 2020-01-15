c93c19a71c210257e7eeb064422549dd827c969b
nickolay <bodan.nick007@gmail.com>
2020-01-15 19:51:01 +0100

    fixed init data, fixed some translates

--- src/main/webapp/app/osh-core/profile/profile.module.ts
+++ src/main/webapp/app/osh-core/profile/profile.module.ts
@@ -6,13 +6,15 @@ import { JhiLanguageService } from 'ng-jhipster';
 import { ProfileComponent } from 'app/osh-core/profile/containers/profile.component';
 import { profileRoute } from 'app/osh-core/profile/profile.route';
 import { OshProfileService } from 'app/osh-core/profile/shared/profile.service';
+import { ObjectSharingSharedModule } from 'app/shared/shared.module';
 const PROFILE_ROUTES = [...profileRoute];
 const CONTAINERS = [ProfileComponent];
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    RouterModule.forChild(PROFILE_ROUTES)
+    RouterModule.forChild(PROFILE_ROUTES),
+    ObjectSharingSharedModule
   ],
   exports: [], // need add ui module
   declarations: [...CONTAINERS],
